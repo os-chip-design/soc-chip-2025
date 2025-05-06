ae0ad912a607a15b3d4052bcfe076b0104b193f7
AsgerWenneb <asger@wenneberg.net>
2025-05-06 09:18:20 +0200

    mem power, define wire and change placement

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -88,9 +88,7 @@ wire [3:0] wmask0;
 wire [9:0] addr0;
 wire [31:0] din0;
 wire [31:0] dout0;
-// wire [31:0] unused_dout1;
-
-// assign dout0 = 0;
+wire [31:0] unused_dout1;
 
 sky130_sram_2kbyte_1rw1r_32x512_8 OpenRAM_inst1 (
 `ifdef USE_POWER_PINS
